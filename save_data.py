7aac7b220ea4ff134a6c0dad00d225223c05d4c0
humster <park.ksenia23@gmail.com>
2022-07-19 21:42:37 +0600

    .

---
+++ save_data.py
@@ -0,0 +1,16 @@
+import csv
+
+def write_to_csv(data):
+    with open('data.csv','w') as file:
+        columns=["название","описание","цена","картинка"]
+        writer=csv.DictWriter(file, columns)
+        writer.writeheader()
+        for prod in data:
+            writer.writerow({
+                "название":prod['title'],
+                "описание":prod['describtion'],
+                "цена":prod['price'],
+                "картинка":prod['image']
+            })
+
+        
